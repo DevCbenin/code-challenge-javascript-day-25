9d852a08e5fe5b9b6882b680f8945b798b25493a
Damilare <32745404+veectorh@users.noreply.github.com>
2020-06-01 11:03:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 
-const abilityModifier = c => {
- //type code here
-}
+const largestProduct = (numX, numY) => {
+if (numX.length < numY) throw new Error ('Span must be smaller than string length');
+if (numY < 0) throw new Error ('Span must be greater than zero');
+if (/\D/.test(numX)) throw new Error ('Digits input must only contain digits');
+if (numY == 0) return 1;
 
-const dice = () => Math.floor(Math.random() * 6) + 1
-
-//restructure code accordingly
-const Character {
-  //type code here
-}
-
-
-module.exports = {abilityModifier, Character}
+  let maxProd = 0;
+  for ( let i = 0; i < numX.length - (numY - 1) ; i++){
+    let currProd = numX
+                        .slice(i, numY + i)
+                        .split("")
+                        .reduce((acc,curr)=>acc*curr,1);
+    if (currProd > maxProd) maxProd = currProd;
+  }
+  return maxProd;
+};
+module.exports = largestProduct

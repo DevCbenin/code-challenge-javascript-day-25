9567eb2ca36ac53bfa9eed811e7c53fe7452969f
Damilare <32745404+veectorh@users.noreply.github.com>
2020-06-01 11:06:42 +0100

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,119 +1,75 @@
 
 
-const { Character, abilityModifier } = require('./index');
+const largestProduct = require('./index');
 
-describe('D&D Character', () => {
-  describe('Ability modifier', () => {
-    test('ability modifier for score 3 is -4', () => {
-      expect(abilityModifier(3)).toEqual(-4);
-    });
-
-    test('ability modifier for score 4 is -3', () => {
-      expect(abilityModifier(4)).toEqual(-3);
-    });
-
-    test('ability modifier for score 5 is -3', () => {
-      expect(abilityModifier(5)).toEqual(-3);
-    });
-
-    test('ability modifier for score 6 is -2', () => {
-      expect(abilityModifier(6)).toEqual(-2);
-    });
-
-    test('ability modifier for score 7 is -2', () => {
-      expect(abilityModifier(7)).toEqual(-2);
-    });
-
-    test('ability modifier for score 8 is -1', () => {
-      expect(abilityModifier(8)).toEqual(-1);
-    });
-
-    test('ability modifier for score 9 is -1', () => {
-      expect(abilityModifier(9)).toEqual(-1);
-    });
-
-    test('ability modifier for score 10 is 0', () => {
-      expect(abilityModifier(10)).toEqual(0);
-    });
-
-    test('ability modifier for score 11 is 0', () => {
-      expect(abilityModifier(11)).toEqual(0);
-    });
-
-    test('ability modifier for score 12 is 1', () => {
-      expect(abilityModifier(12)).toEqual(1);
-    });
-
-    test('ability modifier for score 13 is 1', () => {
-      expect(abilityModifier(13)).toEqual(1);
-    });
-
-    test('ability modifier for score 14 is 2', () => {
-      expect(abilityModifier(14)).toEqual(2);
-    });
-
-    test('ability modifier for score 15 is 2', () => {
-      expect(abilityModifier(15)).toEqual(2);
-    });
-
-    test('ability modifier for score 16 is 3', () => {
-      expect(abilityModifier(16)).toEqual(3);
-    });
-
-    test('ability modifier for score 17 is 3', () => {
-      expect(abilityModifier(17)).toEqual(3);
-    });
-
-    test('ability modifier for score 18 is 4', () => {
-      expect(abilityModifier(18)).toEqual(4);
-    });
-
-    test('ability score less than 3 throws error', () => {
-      expect(() => abilityModifier(2)).toThrow(
-        new Error('Ability scores must be at least 3')
-      );
-    });
-
-    test('ability score greater than 18 throws error', () => {
-      expect(() => abilityModifier(19)).toThrow(
-        new Error('Ability scores can be at most 18')
-      );
-    });
+describe('Largest Series Product', () => {
+  test('finds the largest product if span equals length', () => {
+    expect(largestProduct('29', 2)).toEqual(18);
   });
 
-  describe('Ability scores calculated properly', () => {
-    test('ability within range', () => {
-      expect(Character.rollAbility()).toBeLessThanOrEqual(18);
-      expect(Character.rollAbility()).toBeGreaterThanOrEqual(3);
-    });
-
-    test('ability scores only calculated once', () => {
-      const Drizzt = new Character();
-
-      expect(Drizzt.strength).toEqual(Drizzt.strength);
-    });
+  test('can find the largest product of 2 with numbers in order', () => {
+    expect(largestProduct('0123456789', 2)).toEqual(72);
   });
 
-  describe('Random character is valid', () => {
-    test('character is valid', () => {
-      const Drizzt = new Character();
+  test('can find the largest product of 2', () => {
+    expect(largestProduct('576802143', 2)).toEqual(48);
+  });
 
-      expect(Drizzt.strength).toBeLessThanOrEqual(18);
-      expect(Drizzt.strength).toBeGreaterThanOrEqual(3);
-      expect(Drizzt.dexterity).toBeLessThanOrEqual(18);
-      expect(Drizzt.dexterity).toBeGreaterThanOrEqual(3);
-      expect(Drizzt.constitution).toBeLessThanOrEqual(18);
-      expect(Drizzt.constitution).toBeGreaterThanOrEqual(3);
-      expect(Drizzt.intelligence).toBeLessThanOrEqual(18);
-      expect(Drizzt.intelligence).toBeGreaterThanOrEqual(3);
-      expect(Drizzt.wisdom).toBeLessThanOrEqual(18);
-      expect(Drizzt.wisdom).toBeGreaterThanOrEqual(3);
-      expect(Drizzt.charisma).toBeLessThanOrEqual(18);
-      expect(Drizzt.charisma).toBeGreaterThanOrEqual(3);
+  test('can find the largest product of 3 with numbers in order', () => {
+    expect(largestProduct('0123456789', 3)).toEqual(504);
+  });
 
-      expect(Drizzt.hitpoints).toEqual(
-        10 + abilityModifier(Drizzt.constitution)
-      );
-    });
+  test('can find the largest product of 3', () => {
+    expect(largestProduct('1027839564', 3)).toEqual(270);
+  });
+
+  test('can find the largest product of 5 with numbers in order', () => {
+    expect(largestProduct('0123456789', 5)).toEqual(15120);
+  });
+
+  test('can get the largest product of a big number', () => {
+    expect(
+      largestProduct('73167176531330624919225119674426574742355349194934', 6)
+    ).toEqual(23520);
+  });
+
+  test('reports zero if the only digits are zero', () => {
+    expect(largestProduct('0000', 2)).toEqual(0);
+  });
+
+  test('reports zero if all spans include zero', () => {
+    expect(largestProduct('99099', 3)).toEqual(0);
+  });
+
+  test('rejects span longer than string length', () => {
+    expect(() => largestProduct('123', 4)).toThrow(
+      new Error('Span must be smaller than string length')
+    );
+  });
+
+  test('reports 1 for empty string and empty product (0 span)', () => {
+    expect(largestProduct('', 0)).toEqual(1);
+  });
+
+  test('reports 1 for nonempty string and empty product (0 span)', () => {
+    expect(largestProduct('123', 0)).toEqual(1);
+  });
+
+  test('rejects empty string and nonzero span', () => {
+    expect(() => largestProduct('', 1)).toThrow(
+      new Error('Span must be smaller than string length')
+    );
+  });
+
+  test('rejects invalid character in digits', () => {
+    expect(() => largestProduct('1234a5', 2)).toThrow(
+      new Error('Digits input must only contain digits')
+    );
+  });
+
+  test('rejects negative span', () => {
+    expect(() => largestProduct('12345', -1)).toThrow(
+      new Error('Span must be greater than zero')
+    );
   });
 });
